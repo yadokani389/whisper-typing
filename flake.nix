eb2e1680f0e9f1341cbe172329bf808b70548267
yadokani389 <yadokani389@gmail.com>
2025-07-16 09:32:38 +0900

    enhance: separate server and client dependencies

--- flake.nix
+++ flake.nix
@@ -10,54 +10,72 @@
     }:
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs {
+      pkgs_server = import nixpkgs {
         inherit system;
         config.allowUnfree = true;
         config.cudaSupport = true;
       };
-      python3 = pkgs.python3.withPackages (
+      pkgs_client = import nixpkgs {
+        inherit system;
+      };
+      server_ps =
         ps: with ps; [
           faster-whisper
+          fastapi
+          uvicorn
+          python-multipart
+        ];
+      client_ps =
+        ps: with ps; [
           sounddevice
           soundfile
           pyperclip
-          fastapi
-          uvicorn
           requests
-          python-multipart
           numpy
           setproctitle
-        ]
-      );
-      mkApp = app: file: {
-        type = "app";
-        program = pkgs.lib.getExe (
-          pkgs.writeShellApplication {
-            name = "whisper-typing-${app}";
-            runtimeInputs = [
-              python3
-              pkgs.wtype
-            ];
-            text = ''
-              #!${pkgs.runtimeShell}
-              exec ${python3}/bin/python ${file} "$@"
-            '';
-          }
-        );
-      };
+        ];
+      python3_server = pkgs_server.python3.withPackages server_ps;
+      python3_client = pkgs_client.python3.withPackages client_ps;
+      python3_combined = pkgs_server.python3.withPackages (ps: (server_ps ps) ++ (client_ps ps));
     in
     {
       apps.${system} = {
-        server = mkApp "server" ./server.py;
-        client = mkApp "client" ./client.py;
+        server = {
+          type = "app";
+          program = pkgs_server.lib.getExe (
+            pkgs_server.writeShellApplication {
+              name = "whisper-typing-server";
+              text = ''
+                #!${pkgs_server.runtimeShell}
+                exec ${python3_server}/bin/python ${./server.py} "$@"
+              '';
+            }
+          );
+        };
+
+        client = {
+          type = "app";
+          program = pkgs_client.lib.getExe (
+            pkgs_client.writeShellApplication {
+              name = "whisper-typing-client";
+              runtimeInputs = [
+                pkgs_client.wtype
+              ];
+              text = ''
+                #!${pkgs_client.runtimeShell}
+                exec ${python3_client}/bin/python ${./client.py} "$@"
+              '';
+            }
+          );
+        };
       };
 
-      devShells.${system}.default = pkgs.mkShell {
+      devShells.${system}.default = pkgs_server.mkShell {
         packages = [
-          python3
-          pkgs.wtype
+          python3_combined
+          pkgs_server.wtype
         ];
-        PYTHONPATH = "${python3}/${python3.sitePackages}";
+        PYTHONPATH = "${python3_combined}/${python3_combined.sitePackages}";
       };
     };
 }
